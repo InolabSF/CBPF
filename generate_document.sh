994439ca8b63f6c48ab15a032e1bdc17c6b5239e
kenzan8000 <kenzan8000@gmail.com>
2015-05-11 16:56:13 -0700

    update documents

--- generate_document.sh
+++ generate_document.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 apidoc -i server/app/controllers -o document/CCPF/server/API
+chmod 755 document/CCPF/server/API/index.html
 apidoc -i document/AirCasting/server/API -o document/AirCasting/server/API
+chmod 755 document/AirCasting/server/API/index.html
 apidoc -i document/SmartCitizen/server/API -o document/SmartCitizen/server/API
+chmod 755 document/SmartCitizen/server/API/index.html
 #appledoc --project-name CCPF --project-company ISI-Dentsu of America,Inc. --create-html --no-create-docset --output document/CCPF/iOS/iPhone iOS/HyperMediaAD/Classes
 #appledoc --project-name CCPF --project-company ISI-Dentsu of America,Inc. --create-html --no-create-docset --output document/CCPF/iOS/AppleWatch iOS/HyperMediaAD\ WatchKit\ Extension/Classes
