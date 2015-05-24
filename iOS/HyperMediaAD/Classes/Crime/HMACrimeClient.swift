07f892d11bf2c56c5c6c74499be9cb5a911cf1a8
kenzan8000 <kenzan8000@gmail.com>
2015-05-23 23:14:18 -0700

    add HMACrime

---
+++ iOS/HyperMediaAD/Classes/Crime/HMACrimeClient.swift
@@ -0,0 +1,15 @@
+import Foundation
+
+
+/// MARK: - HMACrimeClient
+class HMACrimeClient: AnyObject {
+
+    /// MARK: - property
+
+    /// MARK: - class method
+
+    static let sharedInstance = HMACrimeClient()
+
+
+    /// MARK: - public api
+}
